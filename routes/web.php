bd9c6cc80e101509ad0bc3fe15840355ddff9c0e
selyraa <selyra.123@gmail.com>
2023-05-05 11:50:02 +0700

    membuat form unggah berkas article

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 
 Route::resource('mahasiswas', MahasiswaController::class);
 Route::post('cari', [MahasiswaController::class, 'cari'])->name('cari');
 Route::get('mahasiswa/nilai/{nim}', [MahasiswaController::class, 'nilai'])->name('nilai');
+
+Route::resource('articles', ArticleController::class);
 
 Route::get('/', function () {
     return view('welcome');
