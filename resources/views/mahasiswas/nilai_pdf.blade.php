3e55c91934676498d49f42e24ca40762bb891833
selyraa <selyra.123@gmail.com>
2023-05-06 23:16:16 +0700

    edit view nilai_pdf

--- resources/views/mahasiswas/nilai_pdf.blade.php
+++ resources/views/mahasiswas/nilai_pdf.blade.php
@@ -6,8 +6,13 @@
         <title>KHS Mahasiswa</title>
     </head>
     <body>
-        <h2>KARTU HASIL STUDI (KHS)</h2>
-        <h1>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h1>
+        <div class="row">
+            <div class="col-lg-12 margin-tb" style="justify-content: center; align-items: center; text-align: center;">
+            <div class="pull-left mt-2">
+                <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
+                <h1 style="margin-top: 30px">KARTU HASIL STUDI (KHS)</h1>
+            </div>
+        </div>
         <div class="left-text" style="margin-top: 40px">
             <p><b>Nama : </b>{{$mahasiswas->nama}}</p>
             <p><b>NIM : </b>{{$mahasiswas->nim}}</p>
